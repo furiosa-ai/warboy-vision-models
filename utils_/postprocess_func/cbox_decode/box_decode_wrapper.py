cc0f9f41b03579acb021192393141be257185394
ubuntu <jongwook.kim@furiosa.ai>
2024-04-03 17:15:15 +0900

    fix: error

--- utils_/postprocess_func/cbox_decode/box_decode_wrapper.py
+++ utils_/postprocess_func/cbox_decode/box_decode_wrapper.py
@@ -1,9 +1,11 @@
-import os
 import ctypes
-import numpy as np
+import os
 from typing import List, Union
 
+import numpy as np
+
 _clib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "cbox_decode.so"))
+
 
 def _init():
     u8 = ctypes.c_uint8
@@ -25,18 +27,18 @@ def _init():
     """
 
     _clib.yolov8_box_decode_feat.argtypes = [
-        f32,   # stride
-        f32,   # conf_thres
-        u32,   # max_boxes
+        f32,  # stride
+        f32,  # conf_thres
+        u32,  # max_boxes
         f32p,  # feat_box
         f32p,  # feat_cls
         f32p,  # feat_extra
-        u32,   # batch_size
-        u32,   # ny
-        u32,   # nx
-        u32,   # nc
-        u32,   # reg_max
-        u32,   # n_extra
+        u32,  # batch_size
+        u32,  # ny
+        u32,  # nx
+        u32,  # nc
+        u32,  # reg_max
+        u32,  # n_extra
         f32p,  # out_batch
         u32p,  # out_batch_pos
     ]
@@ -44,19 +46,20 @@ def _init():
 
     _clib.yolov5_box_decode_feat.argtypes = [
         f32p,  # anchors
-        u32,   # num_anchors
-        f32,   # stride
-        f32,   # conf_thres
-        u32,   # max_boxes
+        u32,  # num_anchors
+        f32,  # stride
+        f32,  # conf_thres
+        u32,  # max_boxes
         f32p,  # feat
-        u32,   # batch_size
-        u32,   # ny
-        u32,   # nx
-        u32,   # no
+        u32,  # batch_size
+        u32,  # ny
+        u32,  # nx
+        u32,  # no
         f32p,  # out_batch
         u32p,  # out_batch_pos
     ]
     _clib.yolov5_box_decode_feat.restype = None
+
 
 def sigmoid(x: np.ndarray) -> np.ndarray:
     # pylint: disable=invalid-name
@@ -64,7 +67,15 @@ def sigmoid(x: np.ndarray) -> np.ndarray:
 
 
 def _yolov8_box_decode_feat(
-    stride, conf_thres, reg_max, max_boxes, feat_box, feat_cls, feat_extra, out_batch, out_batch_pos
+    stride,
+    conf_thres,
+    reg_max,
+    max_boxes,
+    feat_box,
+    feat_cls,
+    feat_extra,
+    out_batch,
+    out_batch_pos,
 ):
     feat_box = feat_box.transpose(0, 2, 3, 1)
     feat_cls = feat_cls.transpose(0, 2, 3, 1)
@@ -103,7 +114,14 @@ def _yolov8_box_decode_feat(
         raise Exception(type(feat_box))
 
 
-def yolov8_box_decode(stride: np.ndarray, conf_thres: float, reg_max: int, feats_box:np.ndarray, feats_cls:np.ndarray, feats_extra: Union[np.ndarray, None] = None) -> List[np.ndarray]:
+def yolov8_box_decode(
+    stride: np.ndarray,
+    conf_thres: float,
+    reg_max: int,
+    feats_box: np.ndarray,
+    feats_cls: np.ndarray,
+    feats_extra: Union[np.ndarray, None] = None,
+) -> List[np.ndarray]:
     bs = feats_box[0].shape[0]
     max_boxes = int(1e4)
     n_extra = feats_extra[0].shape[1] if feats_extra is not None else 0
@@ -128,10 +146,11 @@ def yolov8_box_decode(stride: np.ndarray, conf_thres: float, reg_max: int, feats
     return out_boxes_batched
 
 
-
-def _yolov5_box_decode_feat(anchors, stride, conf_thres, max_boxes, feat, out_batch, out_batch_pos):
+def _yolov5_box_decode_feat(
+    anchors, stride, conf_thres, max_boxes, feat, out_batch, out_batch_pos
+):
     bs, na, ny, nx, no = feat.shape
-    
+
     if isinstance(feat, np.ndarray):
         _clib.yolov5_box_decode_feat(
             np.ascontiguousarray(anchors),
@@ -143,7 +162,7 @@ def _yolov5_box_decode_feat(anchors, stride, conf_thres, max_boxes, feat, out_ba
             bs,
             ny,
             nx,
-            no-5,
+            no - 5,
             out_batch,
             out_batch_pos,
         )
@@ -151,22 +170,26 @@ def _yolov5_box_decode_feat(anchors, stride, conf_thres, max_boxes, feat, out_ba
         raise Exception(type(feat))
 
 
-def yolov5_box_decode(anchors: np.ndarray, stride: np.ndarray, conf_thres:float, feats:np.ndarray) -> List[np.ndarray]:
+def yolov5_box_decode(
+    anchors: np.ndarray, stride: np.ndarray, conf_thres: float, feats: np.ndarray
+) -> List[np.ndarray]:
     bs = feats[0].shape[0]
     max_boxes = int(1e4)
 
     out_batch = np.empty((bs, max_boxes, 6), dtype=np.float32)
     out_batch_pos = np.zeros(bs, dtype=np.uint32)
-    na = len(anchors)
     for l, feat in enumerate(feats):
         bs, _, ny, nx = feat.shape
-        feat = feat.reshape(bs, na, -1, ny, nx).transpose(0,1,3,4,2)
+        feat = feat.reshape(bs, 3, -1, ny, nx).transpose(0, 1, 3, 4, 2)
         _yolov5_box_decode_feat(
             anchors[l], stride[l], conf_thres, max_boxes, feat, out_batch, out_batch_pos
         )
 
-    out_boxes_batched = [boxes[: (pos // 6)] for boxes, pos in zip(out_batch, out_batch_pos)]
+    out_boxes_batched = [
+        boxes[: (pos // 6)] for boxes, pos in zip(out_batch, out_batch_pos)
+    ]
 
     return out_boxes_batched
+
 
 _init()
